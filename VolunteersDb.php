17679b19d3c087aa4566fde9d4177b70953b2989
jayathecoder <89962883+jayathecoder@users.noreply.github.com>
2022-06-27 20:27:15 +0530

    Update VolunteersDb.php

--- VolunteersDb.php
+++ VolunteersDb.php
@@ -16,7 +16,7 @@
     $ConfirmPassword = $_POST['ConfirmPassword'];
 
 	//Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','registration');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
